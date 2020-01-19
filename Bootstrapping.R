80331f5382912d643a32fa54b529fcef6b710924
rprok002 <rprok002@fiu.edu>
2020-01-19 17:41:15 -0500

    Annoycance with r

--- Bootstrapping.R
+++ Bootstrapping.R
@@ -8,3 +8,120 @@ related to predictive microbiolgy have been moved to the package 'nlsMicrobio'
 par(mai=c(1,1,0.1,0.1))
 plot(harv$TIMESTAMP, harv$NEE,
      ylab=expression(paste("NEE (",mu,"mol m"^{-2} ~ s^{-1} ~ ")" )), xlab="")
+plot( NEE ~ PAR, data= day)
+y = nls( NEE ~ (a1 * PAR * ax)/(a1 * PAR + ax) + r, data=day[which(day$MONTH == 07),],
+         start=list(a1= -1 , ax= -1, r= 1),
+         na.action=na.exclude, trace=F, control=nls.control(warnOnly=T))
+
+summary(y)
+
+# Create a function of the model
+lrcModel <- function(PAR, a1, ax, r) {
+  NEE <- (a1 * PAR * ax)/(a1 * PAR + ax) + r
+  return(NEE)
+}
+# Initial: create a function that calculates the initial values of the data sets
+lrc.int <- function (mCall, LHS, data){
+  x <- data$PAR
+  y <- data$NEE
+  r <- max(na.omit(y), na.rm=T) 
+  ax <- min(na.omit(y), na.rm=T) 
+  a1 <- (r + ax)/2 
+  
+  # Create limits for the parameters:
+  a1[a1 > 0]<- -0.1
+  r[r > 50] <- ax*-1
+  r[r < 0] <- 1
+  value = list(a1, ax, r) 
+  names(value) <- mCall[c("a1", "ax", "r")] 
+  return(value)
+}
+#Selftstart function
+SS.lrc <- selfStart(model=lrcModel,initial= lrc.int)
+
+#Find initial values
+iv <- getInitial(NEE ~ SS.lrc('PAR', "a1", "ax", "r"),
+                 data = day[which(day$MONTH == 07),])
+iv
+#Use initial values
+y = nls( NEE ~ (a1 * PAR * ax)/(a1 * PAR + ax) + r, day[which(day$MONTH == 07),],
+         start=list(a1= iv$a1 , ax= iv$ax, r= iv$r),
+         na.action=na.exclude, trace=F, control=nls.control(warnOnly=T))
+summary(y)
+#Here, the model converged. Now, let check assumptions
+res.lrc <- nlsResiduals(y)
+par(mfrow=c(2,2))
+plot(res.lrc, which=1)# Residulas vs fitted values (Constant Variance)
+plot(res.lrc, which=3) # Standardized residuals
+plot(res.lrc, which=4) # Autocorrelation
+plot(res.lrc, which=5) # Histogram (Normality)
+
+#Run bootstrap results
+results <- nlsBoot(y, niter=100 )
+summary(results)
+plot(results, type = "boxplot")
+
+# Dataframe to store parms and se
+parms.Month <- data.frame(
+  MONTH=numeric(),
+  a1=numeric(),
+  ax=numeric(),
+  r=numeric(),
+  a1.pvalue=numeric(),
+  ax.pvalue=numeric(),
+  r.pvalue=numeric(), stringsAsFactors=FALSE, row.names=NULL)
+parms.Month[1:12, 1] <- seq(1,12,1) # Adds months to the file
+
+nee.day <- function(dataframe){ y = nls( NEE ~ (a1 * PAR * ax)/(a1 * PAR + ax) + r, dataframe,
+                                         start=list(a1= iv$a1 , ax= iv$ax, r= iv$r),
+                                         na.action=na.exclude, trace=F,
+                                         control=nls.control(warnOnly=T))
+y.df <- as.data.frame(cbind(t(coef(summary(y)) [1:3, 1]), t(coef(summary(y)) [1:3, 4])))
+names(y.df) <-c("a1","ax", "r", "a1.pvalue", "ax.pvalue", "r.pvalue")
+return (y.df )}
+
+#Write a loop to fit monthly curves and add paramters to a
+#dataframe (parms.Month).
+try(for(j in unique(day$MONTH)){
+  # Determines starting values:
+  iv <- getInitial(NEE ~ SS.lrc('PAR', "a1", "ax", "r"), data = day[which(day$MONTH == j),])
+  # Fits light response curve:
+  y3 <- try(nee.day(day[which(day$MONTH == j),]), silent=T)
+  # Extracts data and saves it in the dataframe
+  try(parms.Month[c(parms.Month$MONTH == j ), 2:7 ] <- cbind(y3), silent=T)
+  rm(y3)
+}, silent=T)
+parms.Month
+
+#Bootstrapping
+# Create file to store parms and se
+boot.NEE <- data.frame(parms.Month[, c("MONTH")]); names (boot.NEE) <- "MONTH"
+boot.NEE$a1.est <- 0
+boot.NEE$ax.est<- 0
+boot.NEE$r.est<- 0
+boot.NEE$a1.se<- 0
+boot.NEE$ax.se<- 0
+boot.NEE$r.se<- 0
+for ( j in unique(boot.NEE$Month)){
+  y1 <-day[which(day$MONTH == j),] # Subsets data
+  # Determines the starting values:
+  iv <- getInitial(NEE ~ SS.lrc('PAR', "a1", "ax", "r"), data = y1)
+  # Fit curve:
+  day.fit <- nls( NEE ~ (a1 * PAR * ax)/(a1 * PAR + ax) + r, data=y1,
+                  start=list(a1= iv$a1 , ax= iv$ax, r= iv$r),
+                  na.action=na.exclude, trace=F, control=nls.control(warnOnly=T))
+  # Bootstrap and extract values:
+  try(results <- nlsBoot(day.fit, niter=100 ), silent=T)
+  try(a <- t(results$estiboot)[1, 1:3], silent=T)
+  try(names(a) <- c('a1.est', 'ax.est', 'r.est'), silent=T)
+  try( b <- t(results$estiboot)[2, 1:3], silent=T)
+  try(names(b) <- c('a1.se', 'ax.se', 'r.se'), silent=T)
+  try(c <- t(data.frame(c(a,b))), silent=T)
+  # Add bootstrap data to dataframe:
+  try(boot.NEE[c(boot.NEE$MONTH == j), 2:7] <- c[1, 1:6], silent=T)
+  try(rm(day.fit, a, b, c, results, y1), silent=T)
+}
+lrc <- merge( parms.Month, boot.NEE, by.x="MONTH", by.y="MONTH") # Merge dataframes
+
+#Challenge
+#Create a dataframe to store month parameter values (parms.Month.night).
