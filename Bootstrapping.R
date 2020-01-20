c4d2dcbe540412ae0f7cb3f67dcffd27cdf7eb6a
rprok002 <rprok002@fiu.edu>
2020-01-19 21:39:36 -0500

    Used starter to get initial values Used values to make model Used model to bootstrap more values New values didn't fit model, model failed for apr-oct Subset data by month

--- Bootstrapping.R
+++ Bootstrapping.R
@@ -125,3 +125,190 @@ lrc <- merge( parms.Month, boot.NEE, by.x="MONTH", by.y="MONTH") # Merge datafra
 
 #Challenge
 #Create a dataframe to store month parameter values (parms.Month.night).
+# Selfstart for the trc:
+trcModel <- function(TA, a, b) {
+  y=a * exp(b-TA)
+  return(y)
+}
+# Create a function to find initial values for the selfstart function:
+trc.int <- function (mCall, LHS, data){
+  x <- data$TA
+  y <- data$NEE
+  
+  a <-1.00703982 + -0.08089044* (min(na.omit(y)))
+  b <- 0.051654 + 0.001400 * (min(na.omit(y)))
+  
+  value = list(a, b)
+  names(value) <- mCall[c("a", "b")]
+  return(value)
+}
+# Selfstart Function
+SS.trc <- selfStart(model=trcModel,initial= trc.int)    
+#Create a dataframe to store month parameter values (parms.Month.night).
+iv <- getInitial(NEE ~ SS.trc('TA', "a", "b"),
+                 data = night[which(night$MONTH == 07),])
+iv <- getInitial(NEE ~ SS.trc('TA', "a", "b"),
+                 data = night[which(night$MONTH == c(1,2,3,4,5,6,7,8,9,10,11,12)),])
+iv
+#nls for new data set
+y = nls( NEE ~ a * exp(b-TA), night[which(night$MONTH ==c(11,9)),],
+         start=list(a= iv$a , b= iv$b),
+         na.action=na.exclude, trace=F, control=nls.control(warnOnly=T))
+summary(y)
+        
+# Sparkle questions:
+#for the methods, is it the workflow steps and then refer to code in an appendix or something?
+#why do we need photosynthetic potential for night data?
+#is the first section needed (starting values plugging back into model?) That's not in the workflow but seems necessary
+#where are the a and b values that the first workflow step refers to? For example, the parms.Month has the ax and a1 and such that is needed for the first workflow of the exercise, but where are the a and b values supposed to be?
+#Warning message:
+#In nls(NEE ~ a * exp(b - TA), night[which(night$MONTH == 7), ],  :
+        # singular gradient
+       #> summary(y)
+       #Error in chol2inv(object$m$Rmat()) : 
+        # element (1, 1) is zero, so the inverse cannot be computed
+#Thoughts?
+
+# Create Dataframe to store the data: Creates a blank data frame for data to be entered into
+
+parms.Month <- data.frame(
+  
+  MONTH=numeric(),
+  
+  a=numeric(),
+  
+  b=numeric(),
+  
+  a.pvalue=numeric(),
+  
+  b.pvalue=numeric(), stringsAsFactors=FALSE, row.names=NULL)
+
+parms.Month[1:12, 1] <- seq(1,12,1) # Creates time file to merge with parm file
+
+#Write a function to fit the model and extract paramters
+#(nee.night).
+
+
+#Functions: based off of initial values
+
+nee.night <- function(dataframe){y.df = nls(NEE ~ a * exp(b-TA),
+                                            
+                                            dataframe, start=list(a= iv$a , b=iv$b ),
+                                            
+                                            na.action=na.exclude, trace=F,
+                                            
+                                            control=nls.control(warnOnly=T))
+
+y.df <- as.data.frame(cbind(t(coef(summary(y.df))[1:2, 1]), t(coef(summary(y.df)) [1:2, 4])))
+
+
+
+names(y.df) <- c("a", "b", "a.pvalue", "b.pvalue")                     
+
+return(y.df)}
+# This loop fits monthly models (1:12):
+#Bootstraps to create more data based off of function created by initial values to see if created data still fits the model
+
+try(for(j in unique(night$MONTH)){
+  
+  print(j)
+  
+  
+  
+  iv <- getInitial(NEE ~ SS.trc('TA', "a", "b"), data = night[which(night$MONTH == j),])
+  
+  
+  
+  y4 <- try(nee.night(night[which(night$MONTH == j),]), silent=T) # Fit night model
+  
+  
+  
+  try(parms.Month[c(parms.Month$MONTH == j ), 2:5 ] <- cbind(y4), silent=T)
+  
+  
+  
+  rm(y4)
+  
+}, silent=T)
+#bunch of warnings because code broke at a=iv$a, meaning loop based on initial values and bootstrapping didn't work
+#created data doesn't fit the model
+
+# Create file to store parms and se: maybe making dataframe to store data created by bootstrapping?
+#File name is boo.NEE
+#Created a file with the column Month and 4 other columns
+boot.NEE <- data.frame(parms.Month[, c("MONTH")]); names (boot.NEE) <- "MONTH"
+
+boot.NEE$a.est<- 0
+
+boot.NEE$b.est<- 0
+
+boot.NEE$a.se<- 0
+
+boot.NEE$b.se<- 0
+
+# Night Model:
+
+for ( j in unique(boot.NEE$MONTH)){
+  
+  print(j)
+  
+  y1 <-night[which(night$MONTH == j),]
+  
+  
+  
+  iv <- getInitial(NEE ~ SS.trc('TA',"a", "b"), data = y1)
+  
+  
+  
+  night.fit <- nls(NEE ~ a * exp(b-TA),
+                   
+                   data=y1, start=list(a= iv$a , b=iv$b ),
+                   
+                   na.action=na.exclude, trace=F,
+                   
+                   control=nls.control(warnOnly=T))
+  
+  
+  
+  results <- nlsBoot(night.fit, niter=100 )
+  
+  a <- t(results$estiboot)[1, 1:2]
+  
+  names(a) <- c('a.est', 'b.est')
+  
+  b <- t(results$estiboot)[2, 1:2]
+  
+  names(b) <- c('a.se', 'b.se')
+  
+  c <- t(data.frame(c(a,b)))
+  
+  boot.NEE[c(boot.NEE$MONTH == j), 2:5] <- c[1, 1:4]
+  
+  rm(day.fit, a, b, c, results, y1)
+  
+}
+
+
+
+trc <- merge( parms.Month, boot.NEE)
+#new trc with the zeros and the p values and such 
+trc
+
+#subset the data by month
+jan <- subset(night, night$MONTH==01)
+feb <- subset(night, night$MONTH==02)
+mar <- subset(night, night$MONTH==03)
+apr <- subset(night, night$MONTH==04)
+may <- subset(night, night$MONTH==05)
+jun <- subset(night, night$MONTH==06)
+jul <- subset(night, night$MONTH==07)
+aug <- subset(night, night$MONTH==08)
+sep <- subset(night, night$MONTH==09)
+oct <- subset(night, night$MONTH==10)
+nov <- subset(night, night$MONTH==11)
+dec <- subset(night, night$MONTH==12)
+
+#determine what TA values work for model
+#know from running model before that apr-oct don't work in the model
+
+
