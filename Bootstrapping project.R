5db36ca7ec4c86ba166091e7d0044aa481770361
rprok002 <rprok002@fiu.edu>
2020-01-22 22:02:21 -0500

    I can't even, I am so done

--- Bootstrapping project.R
+++ Bootstrapping project.R
@@ -25,19 +25,9 @@ trc.int <- function (mCall, LHS, data){
 # Selfstart Function
 SS.trc <- selfStart(model=trcModel,initial= trc.int)
 
-try(for(j in unique(day$MONTH)){
 
-# Determines starting values:
-iv <- getInitial(NEE ~ SS.trc('TA', "a", "b"), data = night[which(night$MONTH == j),])
-# Fits night response curve:
-y4 <- try(nee.night(night[which(night$MONTH == j),]), silent=T)
-# Extracts data and saves it in the dataframe
-try(parms.Month.night[c(parms.Month.night$MONTH == j ), 2:7 ] <- cbind(y4), silent=T)
-rm(y4) 
-}, silent=T)
-parms.Month.night
 
-parms.Month.night <- data.frame(
+parms.Month <- data.frame(
   
   MONTH=numeric(),
   
@@ -49,7 +39,7 @@ parms.Month.night <- data.frame(
   
   b.pvalue=numeric(), stringsAsFactors=FALSE, row.names=NULL)
 
-parms.Month.night[1:12, 1] <- seq(1,12,1) # Creates time file to merge with parm file
+parms.Month [1:12, 1] <- seq(1,12,1) # Creates time file to merge with parm file
 
 nee.night <- function(dataframe){y.df = nls(NEE ~ a * exp(b*TA),
                                             
@@ -67,44 +57,44 @@ return(y.df)}
 
 # This loop fits monthly models (1:12):
 #Bootstraps to create more data based off of function created by initial values to see if created data still fits the model
-try(for(j in unique(night$MONTH)){
-  
-  # Determines starting values:
-  iv <- getInitial(NEE ~ SS.trc('TA', "a", "b"), data = night[which(night$MONTH == j),])
-  # Fits night response curve:
-  y4 <- try(nee.night(night[which(night$MONTH == j),]), silent=T)
-  # Extracts data and saves it in the dataframe
-  try(parms.Month.night[c(parms.Month.night$MONTH == j ), 2:5 ] <- cbind(y4), silent=T)
-  rm(y4)
-}, silent=T)
-parms.Month.night
 
+try(for(j in unique(night$MONTH)){
+# Determines starting values:
+iv <- getInitial(NEE ~ SS.trc('TA', "a", "b"), data = night[which(night$MONTH == j),])
+# Fits night response curve:
+y4 <- try(nee.night(night[which(night$MONTH == j),]), silent=T)
+# Extracts data and saves it in the dataframe
+try(parms.Month[c(parms.Month$MONTH == j ), 2:5 ] <- cbind(y4), silent=T)
+rm(y4)
+}, silent=T)
+
+parms.Month
 
 
 
 #Bootstrapping
 # Create file to store parms and se
-boot.NEE.night <- data.frame(parms.Month.night[, c("MONTH")]); names (boot.NEE.night) <- "MONTH"
-boot.NEE.night$a.est <- 0
-boot.NEE.night$b.est<- 0
-boot.NEE.night$a.se<- 0
-boot.NEE.night$b.se<- 0
+boot.NEE <- data.frame(parms.Month[, c("MONTH")]); names (boot.NEE) <- "MONTH"
+boot.NEE$a.est <- 0
+boot.NEE$b.est <- 0
+boot.NEE$a.se <- 0
+boot.NEE$b.se <- 0
 
 
-for ( j in unique(boot.NEE.night$Month)){
-  
-  y5 <-night[which(night$MONTH == j),] # Subsets data
+for ( j in unique(boot.NEE$MONTH)){
+
+  y1 <-night[which(night$MONTH == j),]  #Subsets data
       
-# Determines the starting values:
-iv <- getInitial(NEE ~ SS.trc('TA', "a", "b"), data = y5)
+  # Determines the starting values:
+  iv <- getInitial(NEE ~ SS.trc('TA', "a", "b"), data = y1)
 
-# Fit curve:
-night.fit <- nls( NEE ~ a * exp(b*TA), data=y5,
-                  start=list(a= iv$a , b= iv$b),
-                  na.action=na.exclude, trace=F, control=nls.control(warnOnly=T))
+  # Fit curve:
+  night.fit <- nls( NEE ~ (a * exp(b * TA)), 
+                    data=y1, start=list(a= iv$a , b= iv$b),
+                    na.action=na.exclude, trace=F, 
+                    control=nls.control(warnOnly=T))
 
 # Bootstrap and extract values:
-
   try(results <- nlsBoot(night.fit, niter=100 ), silent=T)
   try(a <- t(results$estiboot)[1, 1:2], silent=T)
   try(names(a) <- c('a.est','b.est'), silent=T)
@@ -112,15 +102,8 @@ night.fit <- nls( NEE ~ a * exp(b*TA), data=y5,
   try(names(b) <- c('a.se','b.se'), silent=T)
   try(c <- t(data.frame(c(a,b))), silent=T)
   # Add bootstrap data to dataframe:
-  try(boot.NEE.night[c(boot.NEE.night$MONTH == j), 2:5] <- c[1, 1:4], silent=T)
-  try(rm(night.fit, a, b, c, results, y5), silent=T)
+  try(boot.NEE[c(boot.NEE$MONTH == j), 2:5] <- c[1, 1:4], silent=T)
+  try(rm(night.fit, a, b, c, results, y1), silent=T)
 }
-trc <- merge( parms.Month.night, boot.NEE.night,by.x="MONTH", by.y="MONTH") # Merge dataframes
+trc <- merge( parms.Month, boot.NEE,by.x="MONTH", by.y="MONTH") # Merge dataframes
 trc
-
-res.trc <- nlsResiduals(trc)
-par(mfrow=c(2,2))
-plot(res.trc, which=1)# Residulas vs fitted values (Constant Variance)
-plot(res.trc, which=3) # Standardized residuals
-plot(res.trc, which=4) # Autocorrelation
-plot(res.trc, which=5) # Histogram (Normality)
