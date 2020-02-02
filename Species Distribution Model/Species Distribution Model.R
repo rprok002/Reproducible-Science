d59915305da2caccc0c70c449f039eb1af2a5501
rprok002 <rprok002@fiu.edu>
2020-02-01 20:28:15 -0500

    Species Distribution Push

--- Species Distribution Model/Species Distribution Model.R
+++ Species Distribution Model/Species Distribution Model.R
@@ -6,7 +6,17 @@ library("maptools")
 library("rgdal")
 library("dismo")
 SDM=read.csv(file.choose())
-bioclim.data <- getData(name = "worldclim",
-                        var = "bio",
-                        res = 2.5,
-                        path = "data/")
+summary(SDM)
+#To make species distribution modeling more streamlined, 
+#it is useful to have an idea of how widely our species is 
+#geographically distributed. We are going to find general 
+#latitudinal and longitudinal boundaries and store this 
+#information for later use:
+max.lat <- ceiling(max(SDM$latitude))
+min.lat <- floor(min(SDM$latitude))
+max.lon <- ceiling(max(SDM$longitude))
+min.lon <- floor(min(SDM$longitude))
+geographic.extent <- extent(x = c(min.lon, max.lon, min.lat, max.lat))
+
+#Before we do any modeling, it is also a good idea to run a reality check on your occurrence data 
+#by plotting the points on a map.
