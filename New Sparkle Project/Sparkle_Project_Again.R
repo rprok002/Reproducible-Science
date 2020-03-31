b295b19781f037ed2b2c3020aa0fe5297d4caad0
rprok002 <rprok002@fiu.edu>
2020-03-30 22:25:53 -0400

    Continued slogging through

--- New Sparkle Project/Sparkle_Project_Again.R
+++ New Sparkle Project/Sparkle_Project_Again.R
@@ -60,142 +60,27 @@ plot(lm.Triicelength)
 plot(Triicelength, Triicemass)
 
 
+
 library(nlstools)
-#Create a dataframe to store month parameter values (parms.Month.night).
-# Selfstart for the trc:
-TrifreshModel <- function(TrifreshSA, a, b) {
-  y=a * exp(b*TrifreshSA)
-  return(y)
+TriModel <- function(TrifreshSA, a, b) {
+  Trifreshmass=a * exp(b*TrifreshSA)
+  return(Trifreshmass)
 }
 
-
-
-# Create a function to find initial values for the selfstart function:
-Trifresh.int <- function (mCall, LHS, data){
-  x <- Tri.fresh$SA.after..mm2.
-  y <- Tri.fresh$Mass.mg.
+Tri.int <- function (mCall, LHS, data){
+  TrifreshSA <- Tri.fresh$SA.after..mm2.
+  Trifreshmass <- Tri.fresh$Mass.mg.
+  a <- 0.0003853  
+  b <- -1.4164144  
+ 
   
-  value = list(a, b)
-  names(value) <- mCall[c("a", "b")]
-  return(value)
-}
-
-# Selfstart Function
-SS.Trifresh <- selfStart(model=TrifreshModel,initial= Trifresh.int)
-
-
-
-TrifreshSA.Family <- data.frame(
-  
-  Family=factor(),
-  
-  a=numeric(),
-  
-  b=numeric(),
-  
-  a.pvalue=numeric(),
-  
-  b.pvalue=numeric(), stringsAsFactors=FALSE, row.names=NULL)
-
-TrifreshSA.Family [2:5, 1] <- seq(2,5,1) # Creates time file to merge with parm file
-
-TrifreshSAFamily <- function(dataframe){y.df = nls(Trifreshmass ~ a * exp(b*TrifreshSA),
-                                            
-                                            dataframe, start=list(a= iv$a , b=iv$b ),
-                                            
-                                            na.action=na.exclude, trace=F,
-                                            
-                                            control=nls.control(warnOnly=T))
-
-y.df <- as.data.frame(cbind(t(coef(summary(y.df))[1:2, 1]), t(coef(summary(y.df)) [1:2, 4])))
-
-names(y.df) <- c("a", "b", "a.pvalue", "b.pvalue")                     
-
-return(y.df)}
-
-# This loop fits monthly models (1:12):
-#Bootstraps to create more data based off of function created by initial values to see if created data still fits the model
-
-try(for(j in unique(night$MONTH)){
-  # Determines starting values:
-  iv <- getInitial(NEE ~ SS.trc('TA', "a", "b"), data = night[which(night$MONTH == j),])
-  # Fits night response curve:
-  y4 <- try(nee.night(night[which(night$MONTH == j),]), silent=T)
-  # Extracts data and saves it in the dataframe
-  try(parms.Month[c(parms.Month$MONTH == j ), 2:5 ] <- cbind(y4), silent=T)
-  rm(y4)
-}, silent=T)
-
-parms.Month
-
-
-
-#Bootstrapping
-# Create file to store parms and se
-boot.NEE <- data.frame(parms.Month[, c("MONTH")]); names (boot.NEE) <- "MONTH"
-boot.NEE$a.est <- 0
-boot.NEE$b.est <- 0
-boot.NEE$a.se <- 0
-boot.NEE$b.se <- 0
-
-
-for ( j in unique(boot.NEE$MONTH)){
-  
-  y1 <-night[which(night$MONTH == j),]  #Subsets data
-  
-  # Determines the starting values:
-  iv <- getInitial(NEE ~ SS.trc('TA', "a", "b"), data = y1)
-  
-  # Fit curve:
-  night.fit <- nls( NEE ~ (a * exp(b * TA)), 
-                    data=y1, start=list(a= iv$a , b= iv$b),
-                    na.action=na.exclude, trace=F, 
-                    control=nls.control(warnOnly=T))
-  
-  # Bootstrap and extract values:
-  try(results <- nlsBoot(night.fit, niter=100 ), silent=T)
-  try(a <- t(results$estiboot)[1, 1:2], silent=T)
-  try(names(a) <- c('a.est','b.est'), silent=T)
-  try(b <- t(results$estiboot)[2, 1:2], silent=T)
-  try(names(b) <- c('a.se','b.se'), silent=T)
-  try(c <- t(data.frame(c(a,b))), silent=T)
-  # Add bootstrap data to dataframe:
-  try(boot.NEE[c(boot.NEE$MONTH == j), 2:5] <- c[1, 1:4], silent=T)
-  try(rm(night.fit, a, b, c, results, y1), silent=T)
-}
-trc <- merge( parms.Month, boot.NEE,by.x="MONTH", by.y="MONTH") # Merge dataframes
-trc
-library(xtable)
-library(knitr)
-
-
-
-excelTable(data=trc)
-
-load("~/Desktop/NLM_Workshop.RData")
-library(nlstools)
-lrcModel <- function(PAR, a1, ax, r) {
-  NEE <- (a1 * PAR * ax)/(a1 * PAR + ax) + r
-  return(NEE)
-}
-
-lrc.int <- function (mCall, LHS, data){
-  x <- data$PAR
-  y <- data$NEE
-  r <- max(na.omit(y), na.rm=T) 
-  ax <- min(na.omit(y), na.rm=T) 
-  a1 <- (r + ax)/2 
-  
-  a1[a1 > 0]<- -0.1
-  r[r > 50] <- ax*-1
-  r[r < 0] <- 1
-  value = list(a1, ax, r) 
-  names(value) <- mCall[c("a1", "ax", "r")] 
+  value = list(a,b) 
+  names(value) <- mCall[c("a", "b")] 
   return(value)
 }
 
 
-SS.lrc <- selfStart(model=lrcModel,initial= lrc.int)
+SS.Tri <- selfStart(model=TriModel,initial= Tri.int)
 
 iv <- getInitial(NEE ~ SS.lrc('PAR', "a1", "ax", "r"),
                  data = day[which(day$MONTH == 07),])
