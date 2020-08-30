74447bed94b3ca78eaa5fba8cacf5fca17f4c09f
rprok002 <rprok002@fiu.edu>
2020-08-30 12:19:50 -0400

    Multivariate Assignment 1

--- Multivariate/Assignment_1_R_code.R
+++ Multivariate/Assignment_1_R_code.R
@@ -7,4 +7,25 @@ mean(x1)
 mean(x2)
 var(x1)
 var(x2)
+cov(x1,x2)
+cov(x2,x1)
 cor(x1,x2)
+vector1 <- c(mean(x1), mean(x2))
+meanarray <- array(vector1, dim= c(2,1))
+meanarray
+print(meanarray)
+
+##Problem 1.9
+table2= read.csv(file.choose())
+x1b <- table2[,1]
+x2b <- table2[,2]
+plot(x1b, x2b)
+var(x1b)
+var(x2b)
+cov(x1b,x2b)
+x1brotated <- x1b*cos(26)+x2b*sin(26)
+x2brotated <- -x1b*sin(26)+x2b*cos(26)
+table3 <- data.frame(x1brotated,x2brotated)
+print(table3)
+var(x1brotated)
+var(x2brotated)
