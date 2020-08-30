30b562fdb75a45243b37478a98991fe252a00e5c
rprok002 <rprok002@fiu.edu>
2020-08-30 16:33:18 -0400

    Finished a bit of multivariate

--- Multivariate/Assignment_1_R_code.R
+++ Multivariate/Assignment_1_R_code.R
@@ -20,12 +20,33 @@ table2= read.csv(file.choose())
 x1b <- table2[,1]
 x2b <- table2[,2]
 plot(x1b, x2b)
-var(x1b)
-var(x2b)
-cov(x1b,x2b)
-x1brotated <- x1b*cos(26)+x2b*sin(26)
-x2brotated <- -x1b*sin(26)+x2b*cos(26)
+s11 <- var(x1b)
+s22 <- var(x2b)
+s12 <- cov(x1b,x2b)
+x1brotated <- x1b*cos(0.454)+x2b*sin(0.454)
+x2brotated <- -x1b*sin(0.454)+x2b*cos(0.454)
 table3 <- data.frame(x1brotated,x2brotated)
 print(table3)
-var(x1brotated)
-var(x2brotated)
+s11rot <- var(x1brotated)
+s22rot <- var(x2brotated)
+table4= read.csv(file.choose())
+x1d <- table4[,1]
+x2d <- table4[,2]
+x1drotated <- (x1d*cos(0.454))+(x2d*sin(0.454))
+x2drotated <- ((-x1d*sin(0.454))+(x2d*cos(0.454)))
+x1drotated
+x2drotated
+disfromorigin <- sqrt(((x1drotated^2)/s11rot)+((x2drotated^2)/s22rot))
+disfromorigin
+a <- cos(0.454)^2
+b <- sin(0.454)^2
+c <- (a*s11)+(2*cos(0.454)*sin(0.454)*s12)+(b*s22)
+d <- (a*s22)-(2*cos(0.454)*sin(0.454)*s12)+(b*s11)
+e <- cos(0.454)*sin(0.454)
+a11 <- (a/c)+(b/d)
+a22 <- (b/c)+(a/d)
+a12 <- (e/c)-(e/d)
+disfromorigin2 <- sqrt((a11*(x1d^2))+(2*a12*x1d*x2d)+(a22*(x2d^2)))
+disfromorigin2
+
+##Problem 1.16
