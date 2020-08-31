2c1484de4b38ca84b2d547700ff59d55de06eb68
rprok002 <rprok002@fiu.edu>
2020-08-31 15:43:44 -0400

    Partway thru data

--- Multivariate/Assignment_1_R_code.R
+++ Multivariate/Assignment_1_R_code.R
@@ -50,4 +50,40 @@ disfromorigin2 <- sqrt((a11*(x1d^2))+(2*a12*x1d*x2d)+(a22*(x2d^2)))
 disfromorigin2
 
 ##Problem 1.16
-
+table5= read.csv(file.choose())
+DR = table5[,1]
+R = table5[,2]
+DH = table5[,3]
+H = table5[,4]
+DU = table5[,5]
+U = table5[,6]
+meanDR <- mean(DR)
+meanR <- mean(R)
+meanDH <- mean(DH)
+meanH <- mean(H)
+meanDU <- mean(DU)
+meanU <- mean(U)
+meanvector <- c(meanDR,meanR,meanDH,meanH,meanDU,meanU)
+meanarray <- array(meanvector, dim = c(6,1))
+meanarray
+DR11 <- var(DR)
+DR12 <- cov(DR,R)
+DR13 <- cov(DR,DH)
+DR14 <- cov(DR,H)
+DR15 <- cov(DR,DU)
+DR16 <- cov(DR,U)
+DRvarvector <- c(DR11,DR12,DR13,DR14,DR15,DR16)
+R21 <- cov(R,DR)
+R22 <- var(R)
+R23 <- cov(R,DH)
+R24 <- cov(R,H)
+R25 <- cov(R,DU)
+R26 <- cov(R,U)
+Rvarvector <- c(R21,R22,R23,R24,R25,R26)
+DH31 <- cov(DH,DR)
+DH32 <- cov(DH,R)
+DH33 <- var(DH)
+DH34 <- cov(DH,H)
+DH35 <- cov(DH,DU)
+DH36 <- cov(DH,U)
+DHvarvector <-c(DH31,DH32,DH33,DH34,DH35,DH36)
