85cda598649a7700ee67a167497653e9b95ffdc6
rprok002 <rprok002@fiu.edu>
2020-09-21 16:09:45 -0400

    Problem 1 solved

--- Multivariate/Assignment_2_R_code.R
+++ Multivariate/Assignment_2_R_code.R
@@ -1,4 +1,4 @@
-## Problem 3.2
+## Problem 1: Exercise 3.2
 data1 = read.csv(file.choose())
 p1 = data1[,1]
 p2 = data1[,2]
@@ -9,4 +9,11 @@ data1 = read.csv(file.choose())
 p1 = data1[,1]
 p2 = data1[,2]
 plot(p1,p2)
+invcosangle = acos(-0.866)
+degreesangle = invcosangle*(180/3.1415)
+matrixSn3.2 = matrix(c(2,-3,-3,6), nrow = 2, ncol = 2)
+matrixSn3.2
+corrcoeff3.2 = matrix(c(1,-0.86,-0.86,1), nrow = 2, ncol = 2)
+corrcoeff3.2
 
+## Problem 2: 
