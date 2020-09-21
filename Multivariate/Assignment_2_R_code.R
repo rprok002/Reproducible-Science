6045e021b3c4b57525860c7568880de81967ea13
rprok002 <rprok002@fiu.edu>
2020-09-21 13:30:25 -0400

    slow but sure start

--- Multivariate/Assignment_2_R_code.R
+++ Multivariate/Assignment_2_R_code.R
@@ -1,1 +1,12 @@
-lala
+## Problem 3.2
+data1 = read.csv(file.choose())
+p1 = data1[,1]
+p2 = data1[,2]
+plot(p1,p2)
+meanp1 = mean(p1)
+meanp2 = mean(p2)
+data1 = read.csv(file.choose())
+p1 = data1[,1]
+p2 = data1[,2]
+plot(p1,p2)
+
