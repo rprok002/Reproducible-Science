a8c635d46b6104783edbf2be7251271595607a4f
rprok002 <rprok002@fiu.edu>
2020-10-20 00:29:50 -0400

    Working on data

--- NACCB 2022/NACCB Rcode.R
+++ NACCB 2022/NACCB Rcode.R
@@ -1,2 +1,18 @@
 NACCBlocal = read.csv(file.choose())
 city = NACCBlocal[,1]
+stateprov = NACCBlocal[,2]
+zip = NACCBlocal[,3]
+library(devtools)
+install_github("hrbrmstr/albersusa")
+library(zipcodeR)
+library(tidyverse)
+library(maps)
+library(viridis)
+library(ggthemes)
+library(albersusa)
+data("zip_code_db")
+zip_code_db
+NACCB.zip = aggregate(data.frame(count = zip),list(zip,stateprov),length)
+NACCB.zip.data = merge(NACCB.zip,zip_code_db)
+NACCB.zip.data
+?ggplot
