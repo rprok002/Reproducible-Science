8262415877b42ebca3b31c94c7e1a179d8d514e1
rprok002 <rprok002@fiu.edu>
2020-11-02 17:37:29 -0500

    Problem 4 done??

--- Multivariate/Assignment 4 Rcode.R
+++ Multivariate/Assignment 4 Rcode.R
@@ -98,3 +98,79 @@ intervallow23 = 201.6 - 155.997
 intervallow23
 intervalup23 = 201.6 + 155.997
 intervalup23
+
+## part b
+ctr<-c(74.4, 201.6)
+library(mixtools)
+s1=matrix(c(13825.3, 23823.4, 23823.4,73107.4),2)
+s2=matrix(c(8632, 19616.7, 19616.7,55964.5),2)
+Sp=((45-1)/(45+55-2))*s1+((55-1)/(45+55-2))*s2
+Sp
+A <-Sp # covariance matrix -> cov(dataMatrix)
+RR <- chol(A) # Cholesky decomposition
+angles <- seq(0, 2*pi, length.out=200) # angles for ellipse
+ell <- 1 * cbind(cos(angles), sin(angles)) %*% RR # ellipse scaled with factor 1
+ellCtr <- sweep(ell, 2, ctr, "+") # center ellipse to the data centroid
+plot(ellCtr, type="l", lwd=2, xlab="mu11-mu21",ylab="mu12-m22", asp=1)# plot ellipse
+points(ctr[1], ctr[2], xlab="m11-mu21", pch=4, lwd=2) # plot data centroid
+library(car) # verify with car's ellipse() function
+ellipse(ctr, shape=A, radius=0.98, col="red", lty=2) 
+eigVal <- eigen(A)$values
+eigVec <- eigen(A)$vectors
+eigScl <- eigVec %*% diag(sqrt(eigVal)) # scale eigenvectors to length = square-root
+xMat <- rbind(ctr[1] + eigScl[1, ], ctr[1] - eigScl[1, ])
+yMat <- rbind(ctr[2] + eigScl[2, ], ctr[2] - eigScl[2, ])
+ellBase <- cbind(sqrt(eigVal[1])*cos(angles), sqrt(eigVal[2])*sin(angles)) # normal ellipse
+ellRot <- eigVec %*% t(ellBase) # rotated ellipse
+matlines(xMat, yMat, lty=1, lwd=2, col="green")
+points(ctr[1], ctr[2], pch=4, col="red", lwd=3)
+#abline(h=0, untf=FALSE, lty=1)
+#abline(v=0, untf=FALSE, lty=1)
+
+## Problem 4: Exercise 6.6
+## part a
+treatment2 = matrix(c(3,6,3,3,1,2), nrow = 3, ncol = 2)
+treatment3 = matrix(c(3,1,1,3,2,5,3,2), nrow = 4, ncol = 2)
+cov6.62 = cov(treatment2)
+cov6.63 = cov(treatment3)
+cov6.62
+cov6.63
+Sp6.6a = (((3-1)*cov6.62)+((4-1)*cov6.63))/(3+4-2)
+Sp6.6a
+tstatS = ((1/3)+(1/4))*Sp6.6a
+invtstatS = solve(tstatS)
+meantreatment2.1= mean(treatment2[,1])
+meantreatment2.1
+meantreatment2.2 = mean(treatment2[,2])
+meantreatment3.1 = mean(treatment3[,1])
+meantreatment3.2 = mean(treatment3[,2])
+meantreatment2 = matrix(c(meantreatment2.1,meantreatment2.2), nrow = 2, ncol = 1)
+meantreatment2
+meantreatment3 = matrix(c(meantreatment3.1, meantreatment3.2), nrow = 2, ncol = 1)
+meantreatment3
+difmean6.6a = meantreatment2-meantreatment3
+difmean6.6a
+tdifmean6.6a = t(difmean6.6a)
+tsquared = tdifmean6.6a %*% invtstatS %*% difmean6.6a
+tsquared
+x = ((3+4-2)*2)/(3+4-2-1)
+x
+u = qf(0.99,2,3+4-2-1)
+u
+tcrit6.6 = x*u
+tcrit6.6
+## fail to reject null
+## part c
+relate1S = sqrt(((1/3)*(1/4))*2)
+interval16.6 = sqrt(tcrit6.6)*relate1S
+interval16.6low = 2 - interval16.6
+interval16.6low
+interval16.6up = 2 + interval16.6
+interval16.6up
+relate2S = sqrt(((1/3)*(1/4))*1.6)
+interval26.6 = sqrt(tcrit6.6)*relate2S
+interval26.6low = -1 - interval26.6
+interval26.6low
+interval26.6up = -1 + interval26.6
+interval26.6up
+## both intervals cover 0, which supports failing to reject null
