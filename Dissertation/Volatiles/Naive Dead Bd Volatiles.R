667225d52f740b7ee9b9af981353e30de84c0411
rprok <rprok002@fiu.edu>
2021-12-17 21:04:03 -0500

    Stats done, don't really know what they mean though

--- Dissertation/Volatiles/Naive Dead Bd Volatiles.R
+++ Dissertation/Volatiles/Naive Dead Bd Volatiles.R
@@ -1,9 +1,11 @@
 ## Naive Dead Bd Chemosensory Trials 1 through 3 frogs 1-7, 9-14
 
+naivedeadbdvolatiles <- read.csv(file.choose())
+library(lmerTest)
+library(car)
+naivedeadbdvolatilesglm <-glmer(Weight~ Location*Group +(1|Trial_Number) +(1|Frog_Number) , 
+                                data = naivedeadbdvolatiles, family = binomial)
+anova(naivedeadbdvolatilesglm, type=2)
+car::Anova(naivedeadbdvolatilesglm, type=2)
+summary(naivedeadbdvolatilesglm)
 
-package:
-  library(lmerTest)
-
-Example model:
-  d <-glmer(Diversity~ Adjusted.day*Sex+ Body.Mass+(1|id) +(1|Enclosure) , data = div, family=binomial)
-summary(d)
