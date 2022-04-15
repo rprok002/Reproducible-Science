dad8fe2a55802b08cfe42875b4952ea217da5eca
rprok <rprok002@fiu.edu>
2022-04-15 13:12:55 -0400

    Added columns to data table and dataframe. Now have both complete to make graphs from

--- Zoo Miami Collaboration/Publications graphs.R
+++ Zoo Miami Collaboration/Publications graphs.R
@@ -1,3 +1,4 @@
+## making data table 
 library(data.table)
 pubtable = data.table(
     ID = c("Zoo Miami", "Lion County Safari", "Palm Beach Zoo", "Naples Zoo", 
@@ -11,8 +12,11 @@ pubtable = data.table(
            "Natural Encounters Inc."),
     Threatend_Florida_Species = c(3,0,2,0,197,5,5,0,4,0,13,0,3,16,8,3,2,0,2,7,0,0),
     Conservation_Publications = c(7,0,4,2,308,4,0,0,3,1,9,0,2,1,3,0,2,0,3,8,1,0),
-    Total_Publications = c(38,0,29,17,1201,26,31,0,159,1,50,0,74,27,9,12,7,9,26,117,6,3)
+    Total_Publications = c(38,0,29,17,1201,26,31,0,159,1,50,0,74,27,9,12,7,9,26,117,6,3),
+    Percent_Florida_Species = percentTFS,
+    Percent_Conservation = percentCP
 )
+pubtable[is.na(pubtable)] = 0
 pubtable
 ID <- c("Zoo Miami", "Lion County Safari", "Palm Beach Zoo", "Naples Zoo", 
         "Mote Marine Laboratory and Zoo", "Brevard Zoo", "Seaworld Orlando",
