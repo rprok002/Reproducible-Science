46828d6e8fb30648625f4f338ee00c9eca299f1f
rprok <rprok002@fiu.edu>
2022-04-17 23:09:34 -0400

    Other graph done

--- Zoo Miami Collaboration/Publications graphs.R
+++ Zoo Miami Collaboration/Publications graphs.R
@@ -34,16 +34,20 @@ TP <- c(38,0,29,17,1201,26,31,0,159,1,50,0,74,27,9,12,7,9,26,117,6,3)
 pubtable
 pubdataframe <- data.frame(ID, TFS, CP, TP, percentTFS, percentCP) 
 pubdataframe
-percentTFS <- pubdataframe[,2]/pubdataframe[,4]
+percentTFS <- trunc((pubdataframe[,2]/pubdataframe[,4])*100)
 percentTFS
-percentCP <- pubdataframe[,3]/pubdataframe[,4]
+percentCP <- trunc((pubdataframe[,3]/pubdataframe[,4])*100)
 percentCP
 pubdataframe[is.na(pubdataframe)] = 0
 pubdataframe
 
 library(tidyr)
 pubdataframeTFSCP <- gather(pubdataframe, event, total , TFS:CP)
+pubdataframepercentTFSCP <- gather(pubdataframe, event, total , percentTFS:percentCP)
 pubdataframeTFSCP
+pubdataframepercentTFSCP
+pubdataframepercentTFSCP[is.na(pubdataframepercentTFSCP)] = 0
+pubdataframepercentTFSCP
 ## making the graphs
 library(ggplot2)
 plot <- ggplot(pubdataframeTFSCP, aes(ID, total, fill=event))
@@ -59,4 +63,16 @@ plot <- plot + geom_bar(stat = "identity", position = position_dodge(width=0.7))
   theme(plot.title = element_text(hjust = 0.5))+
   theme(plot.margin = margin(0.5,1,1,1, "cm"))
 plot 
-
+plot2 <- ggplot(pubdataframepercentTFSCP, aes(ID, total, fill=event))
+plot2 <- plot2 + geom_bar(stat = "identity", position = position_dodge(width=0.8)) + theme_bw() +
+  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank()) +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1)) + 
+  geom_text(aes(label=total), position=position_dodge(width=0.95), vjust=-0.25, size = 3)+
+  xlab("Institution")+ ylab("Percent of Total Publications")+ 
+  scale_fill_manual(values = c("chartreuse3", "maroon2"), name = "Legend", labels=c("Conservation", "Florida Threatened Species"))+
+  theme(legend.title = element_text(color = "black", size = 9), 
+        legend.text = element_text(color = "black", size = 8))+
+  ggtitle("Percent of Conservation and Florida Threatened Species Publications \n by Institution")+
+  theme(plot.title = element_text(hjust = 0.5))+
+  theme(plot.margin = margin(0.5,1,1,1, "cm"))
+plot2
