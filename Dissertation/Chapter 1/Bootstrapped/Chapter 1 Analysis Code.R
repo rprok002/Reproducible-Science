5e14612b9fec4d66928d922610178bfa856c9765
rprok <rprok002@fiu.edu>
2022-05-17 00:16:09 -0400

    New data frame for first sample

--- Dissertation/Chapter 1/Bootstrapped/Chapter 1 Analysis Code.R
+++ Dissertation/Chapter 1/Bootstrapped/Chapter 1 Analysis Code.R
@@ -14,5 +14,10 @@ F1T1N1c <- F1T1N1s[which(F1T1N1s$Location == 1),]
 F1T1N1c
 F1T1N1e <- F1T1N1s[which(F1T1N1s$Location == 2),]
 F1T1N1e
-sum(F1T1N1c$Number.of.Seconds)
-sum(F1T1N1e$Number.of.Seconds)
+Control <- sum(F1T1N1c$Number.of.Seconds)
+Experiment <- sum(F1T1N1e$Number.of.Seconds)
+
+Location <- c("C", "E")
+Seconds <- c(Control,Experiment)
+F1T1N1sum <- data.frame(Location, Seconds)
+F1T1N1sum
