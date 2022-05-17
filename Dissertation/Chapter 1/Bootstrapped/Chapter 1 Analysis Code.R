a7afb8e10f4c43b4ae06b50881c28c7ced6635d5
rprok <rprok002@fiu.edu>
2022-05-17 00:05:49 -0400

    Random sample frog 1 trial type 1 trial number 1

---
+++ Dissertation/Chapter 1/Bootstrapped/Chapter 1 Analysis Code.R
@@ -0,0 +1,18 @@
+## load data table
+data <- read.csv(file.choose())
+
+## Filter (F = frog number, T = trial type, N = trial number)
+F1T1N1 <- data[which(data$Frog.Number == 1 & data$Trial.Type == 1 & data$Trial.Number == 1),]
+F1T1N1
+
+## Random sample
+F1T1N1s <- F1T1N1[sample(nrow(F1T1N1), 180),]
+F1T1N1s
+
+## Filter by location
+F1T1N1c <- F1T1N1s[which(F1T1N1s$Location == 1),]
+F1T1N1c
+F1T1N1e <- F1T1N1s[which(F1T1N1s$Location == 2),]
+F1T1N1e
+sum(F1T1N1c$Number.of.Seconds)
+sum(F1T1N1e$Number.of.Seconds)
