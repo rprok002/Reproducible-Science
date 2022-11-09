3414da0c0f888bfccd63488c9c52a1b9c3ad861a
Rachel Prokopius <rprok002@fiu.edu>
2022-11-09 14:13:08 -0500

    Can'tfigure out how to decrease width in columns

--- Dissertation/Chapter 1/ANOVA and binomial/ANOVA and Binomial.R
+++ Dissertation/Chapter 1/ANOVA and binomial/ANOVA and Binomial.R
@@ -241,5 +241,21 @@ ggplot(DeadBdConvar, aes(fill = Group, y = Weight, x = Frog_Number))+
       labs(fill = "Location")+
       scale_fill_manual(values = c("slateblue4", "purple1", "turquoise1"), 
                         labels = c("Side A", "Side C", "Neutral"))
-      
+## Experiment plot
+ggplot(DeadbdExpvar, aes(fill = Group, y = Weight, x = Frog_Number))+
+  geom_bar(position = "stack", stat = "identity", color = "black")+
+  facet_grid(.~Frog_Number, scales = "free_x", switch = "x")+
+  theme_bw()+
+  theme(axis.text.x = element_blank(), axis.ticks.x = element_blank(),
+        plot.background = element_blank(), 
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        panel.border = element_blank(),
+        strip.background = element_rect(fill = "white", colour = "white"),
+        plot.title = element_text(hjust = 0.7))+
+  xlab("Frog Number") + ylab("Proportion of Time") + 
+  ggtitle("Control Frogs: Proportion of Time per Location")+
+  labs(fill = "Location")+
+  scale_fill_manual(values = c("slateblue4", "purple1", "turquoise1"), 
+                    labels = c("Side A", "Side C", "Neutral"))
       
