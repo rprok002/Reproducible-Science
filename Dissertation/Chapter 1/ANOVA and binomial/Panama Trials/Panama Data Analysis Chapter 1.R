f3e9db7083a84a6b5f056cbda541d91b3d00876e
Rachel Prokopius <rachelprokopius@Rachels-MacBook-Pro.local>
2023-06-23 21:58:59 -0500

    Mostly not normal and also mostly equal variances, can still move forward with regression

--- Dissertation/Chapter 1/ANOVA and binomial/Panama Trials/Panama Data Analysis Chapter 1.R
+++ Dissertation/Chapter 1/ANOVA and binomial/Panama Trials/Panama Data Analysis Chapter 1.R
@@ -70,3 +70,99 @@ ggdensity(LearnedLive$Neutral_Fraction, main = "Density Plot of  Neutral", xlab 
 ggqqplot(LearnedLive$Neutral_Fraction)
 ggdensity(LearnedLive$Total_Fraction, main = "Density Plot of Total", xlab = "Total")
 ggqqplot(LearnedLive$Total_Fraction)
+
+## Shapiro-Wilks test
+## Null: normal: Alt: not normal
+shapiro.test(NaiveControl$Side_A_Fraction)
+## not normal
+shapiro.test(NaiveControl$Side_C_Fraction)
+## not normal
+shapiro.test(NaiveControl$Neutral_Fraction)
+## normal
+shapiro.test(NaiveControl$Total_Fraction)
+## normal
+
+shapiro.test(LearnedControl$Side_A_Fraction)
+## normal
+shapiro.test(LearnedControl$Side_C_Fraction)
+## slightly not normal
+shapiro.test(LearnedControl$Neutral_Fraction)
+## normal
+shapiro.test(LearnedControl$Total_Fraction)
+## normal
+
+shapiro.test(NaiveDead$Control_Fraction)
+## normal
+shapiro.test(NaiveDead$Experiment_Fraction)
+## normal
+shapiro.test(NaiveDead$Neutral_Fraction)
+## normal
+shapiro.test(NaiveDead$Total_Fraction)
+## normal
+
+shapiro.test(LearnedDead$Control_Fraction)
+## not normal
+shapiro.test(LearnedDead$Experiment_Fraction)
+## normal
+shapiro.test(LearnedDead$Neutral_Fraction)
+## normal
+shapiro.test(LearnedDead$Total_Fraction)
+## normal
+
+shapiro.test(NaiveLive$Control_Fraction)
+## normal
+shapiro.test(NaiveLive$Experiment_Fraction)
+## not normal
+shapiro.test(NaiveLive$Neutral_Fraction)
+## normal
+shapiro.test(NaiveLive$Total_Fraction)
+## normal
+
+shapiro.test(LearnedLive$Control_Fraction)
+## normal
+shapiro.test(LearnedLive$Experiment_Fraction)
+## slightly normal
+shapiro.test(LearnedLive$Neutral_Fraction)
+## slightly normal
+shapiro.test(LearnedLive$Total_Fraction)
+## normal
+
+## Naive Control: not normal
+## Learned Control: slightly not normal
+## Naive Dead: normal
+## Learned Dead: not normal
+## Naive live: not normal
+## Learned live: normal
+
+## Naive seems less normal than learned, but overall not normally distributed
+
+## Levene's test for equal variance
+## Null: all pop var are equal; Alt: at least two are different
+library(car)
+NaiveControlAnalysis = read.csv(file.choose())
+NaiveDeadAnalysis = read.csv(file.choose())
+NaiveLiveAnalysis = read.csv(file.choose())
+LearnedControlAnalysis = read.csv(file.choose())
+LearnedDeadAnalysis = read.csv(file.choose())
+LearnedLiveAnalysis = read.csv(file.choose())
+
+NaiveControlVar = leveneTest(Weight~Group, NaiveControlAnalysis)
+NaiveControlVar
+## equal variance
+NaiveDeadVar = leveneTest(Weight~Group, NaiveDeadAnalysis)
+NaiveDeadVar
+## equal variance
+NaiveLiveVar = leveneTest(Weight~Group, NaiveLiveAnalysis)
+NaiveLiveVar
+## equal variance
+LearnedControlVar = leveneTest(Weight~Group, LearnedControlAnalysis)
+LearnedControlVar
+## non-equal variance
+LearnedDeadVar = leveneTest(Weight~Group, LearnedDeadAnalysis)
+LearnedDeadVar
+## equal variance
+LearnedLiveVar = leveneTest(Weight~Group, LearnedLiveAnalysis)
+LearnedLiveVar
+## equal variance
+
+## mostly equal variance across the board, except learned control 
