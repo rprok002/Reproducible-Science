3232bd2246dac93ed5770d5cd4cc86ae04148f8d
Rachel Prokopius <rachelprokopius@Rachels-MacBook-Pro.local>
2023-06-23 21:06:37 -0500

    New file for Panama trials, starting to add code

---
+++ Dissertation/Chapter 1/ANOVA and binomial/Panama Trials/Panama Data Analysis Chapter 1.R
@@ -0,0 +1,7 @@
+## Data Analysis Panama Chapter 1
+NaiveControl= read.csv(file.choose())
+NaiveDead= read.csv(file.choose())
+NaiveLive = read.csv(file.choose())
+LearnedControl = read.csv(file.choose())
+LearnedDead = read.csv(file.choose())
+LearnedLive = read.csv(file.choose())
