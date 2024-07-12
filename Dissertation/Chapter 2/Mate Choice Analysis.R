7266c7af2d6b5d6ef23e429a447cd4fdf4a7298c
Rachel Prokopius <rachelprokopius@Rachels-MacBook-Pro.local>
2024-07-12 15:51:10 -0400

    Starting to build control trial models

--- Dissertation/Chapter 2/Mate Choice Analysis.R
+++ Dissertation/Chapter 2/Mate Choice Analysis.R
@@ -3,3 +3,67 @@
 install.packages("ggpubr")
 library(ggpubr)
 library(dplyr)
+
+MateChoiceCompiledDataControlNoCall <- read.csv(file.choose())
+MateChoiceAnalysisControl <- read.csv(file.choose())
+
+ggdensity(MateChoiceCompiledDataControlNoCall$Total_Trial_Time_Seconds, main = "Density Plot of Total Trial Time Seconds", xlab = " Total Trial Time Seconds")
+ggqqplot(MateChoiceCompiledDataControlNoCall$Total_Trial_Time_Seconds)
+## def not normal distribution, but independent variable doesn't need to be 
+
+ggdensity(MateChoiceCompiledDataControlNoCall$Female_Interaction_Zone, main = "Density Plot of Female Interaction Zone", xlab = " Female Interaction Zone")
+ggqqplot(MateChoiceCompiledDataControlNoCall$Female_Interaction_Zone)
+## not normal distribution
+
+ggdensity(MateChoiceCompiledDataControlNoCall$Female_Left, main = "Density Plot of Female Left", xlab = " Female Left")
+ggqqplot(MateChoiceCompiledDataControlNoCall$Female_Left)
+## not normal distribution
+
+ggdensity(MateChoiceCompiledDataControlNoCall$Female_Right, main = "Density Plot of Female Right", xlab = " Female Right")
+ggqqplot(MateChoiceCompiledDataControlNoCall$Female_Right)
+## not normal distribution
+
+ggdensity(MateChoiceCompiledDataControlNoCall$SQRT_Total_Trial_Time_Seconds, main = "Density Plot of SQRT Total Trial Time Seconds", xlab = " SQRT Total Trial Time Seconds")
+ggqqplot(MateChoiceCompiledDataControlNoCall$SQRT_Total_Trial_Time_Seconds)
+## def not normal, but independent variable doesn't need to be
+
+ggdensity(MateChoiceCompiledDataControlNoCall$SQRT_Female_Interaction_Zone, main = "Density Plot of SQRT Female Interaction Zone", xlab = " SQRT Female Interaction Zone")
+ggqqplot(MateChoiceCompiledDataControlNoCall$SQRT_Female_Interaction_Zone)
+## sqrt actually skews more
+
+ggdensity(MateChoiceCompiledDataControlNoCall$SQRT_Female_Left, main = "Density Plot of SQRT Female Left", xlab = " SQRT Female Left")
+ggqqplot(MateChoiceCompiledDataControlNoCall$SQRT_Female_Left)
+## sqrt helps to normalize
+
+ggdensity(MateChoiceCompiledDataControlNoCall$SQRT_Female_Right, main = "Density Plot of SQRT Female Right", xlab = " SQRT Female Right")
+ggqqplot(MateChoiceCompiledDataControlNoCall$SQRT_Female_Right)
+## sqrt helps to normalize
+
+ggdensity(MateChoiceAnalysisControl$Weight_Seconds, main = "Density Plot of Weight Seconds", xlab = " Weight Seconds")
+ggqqplot(MateChoiceAnalysisControl$Weight_Seconds)
+## not normal
+
+ggdensity(MateChoiceAnalysisControl$SQRT_Weight_Seconds, main = "Density Plot of SQRT Weight Seconds", xlab = " SQRT Weight Seconds")
+ggqqplot(MateChoiceAnalysisControl$SQRT_Weight_Seconds)
+## helps to normalize better
+
+ggdensity(MateChoiceAnalysisControl$Time_Interaction_Zone, main = "Density Plot of Time Interaction Zone", xlab = " Time Interaction Zone")
+ggqqplot(MateChoiceAnalysisControl$Time_Interaction_Zone)
+## not normal, but independent doesn't have to be
+
+ggdensity(MateChoiceAnalysisControl$SQRT_Time_Interaction_Zone, main = "Density Plot of SQRT Time Interaction Zone", xlab = " SQRT Time Interaction Zone")
+ggqqplot(MateChoiceAnalysisControl$SQRT_Time_Interaction_Zone)
+## actually makes worse
+
+## using sqrt for Weight Seconds because normalizes so no family need be added for LMER 
+
+## Control Models 
+install.packages("ggpubr")
+library(ggpubr)
+library(dplyr)
+library(lme4)
+library(lmerTest)
+library(emmeans)
+library(multcomp)
+library(nlme)
+library(lmtest)
