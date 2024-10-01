5cc58e5ef47807431f3545cb47a538252248f05b
Rachel Prokopius <rachelprokopius@Rachels-MacBook-Pro.local>
2024-10-01 14:33:57 -0400

    Started R script for final chapter

---
+++ Dissertation/Chapter 3/Panama Carotenoids.R
@@ -0,0 +1,7 @@
+## Carotenoids 
+
+## MANOVA rules####
+## Assumptions:
+## Independent observations: Use intraclass correlation (ICC) in R to test, value is between -1 (strong neg correlation) and 1 (strong positive correlation)
+## Normality: Shapiro-Wilks
+## Equal variance: Box M Test
